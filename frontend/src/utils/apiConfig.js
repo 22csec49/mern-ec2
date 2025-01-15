d4829c56f7405f7198c86f889fa1c6ae8f302e6a
22csec49 <22csec49@gmail.com>
2025-01-15 12:41:17 +0530

    Initial commit with contents of the plant folder

--- frontend/src/utils/apiConfig.js
+++ frontend/src/utils/apiConfig.js
@@ -1,4 +1,4 @@
-export const API_URL = 'http://localhost:4000'
+export const API_URL = 'http://52.64.137.125:4000'
 //export const API_URL = 'https://plant-care-automation-backend.onrender.com';
 //export const API_URL = 'http://192.168.217.5:4000'
 export const maxSoilMoisture = 720;
